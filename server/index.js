5f2aab50115e10332bc49fa7e198c8c4b45b4cc3
Michael Cheng <cheng.c.mike@gmail.com>
2018-09-19 09:28:31 -0500

    basic setup

--- server/index.js
+++ server/index.js
@@ -1,41 +1,58 @@
-const express = require('express');
-const path = require('path');
-const cluster = require('cluster');
-const numCPUs = require('os').cpus().length;
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+mongoose.connect(
+  process.env.MONGODB_URL || "mongodb://localhost/bibleanswers",
+  { useNewUrlParser: true }
+);
 
 const PORT = process.env.PORT || 5000;
 
-// Multi-process to utilize all CPU cores.
-if (cluster.isMaster) {
-  console.error(`Node cluster master ${process.pid} is running`);
+const app = express();
 
-  // Fork workers.
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
+var postSchema = new mongoose.Schema({
+  title: String,
+  subtitle: String,
+  imageUrl: String,
+  imagePosition: String,
+  tags: Array,
+  url: String,
+  added: String,
+  updated: String,
+  content: String
+});
 
-  cluster.on('exit', (worker, code, signal) => {
-    console.error(`Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`);
+var Post = mongoose.model("Post", postSchema);
+
+// var post = new Post({
+//   title: "What is the Gospel?",
+//   subtitle: "Learn the basics of the Bible, God, sin, Jesus, and salvation",
+//   imageUrl: "../../static/cover-gospel.jpeg",
+//   imagePosition: "79% 50%",
+//   tags: ["gospel"],
+//   url: "/gospel",
+//   added: "9/15/18",
+//   content: "Hello world"
+// });
+
+// post.save();
+
+app.use(express.static(path.resolve(__dirname, "../react-ui/build")));
+
+app.get("/api/posts", function(req, res) {
+  res.set("Content-Type", "application/json");
+
+  Post.find({}, (err, posts) => {
+    res.send({
+      posts
+    });
   });
+});
 
-} else {
-  const app = express();
+app.get("*", function(request, response) {
+  response.sendFile(path.resolve(__dirname, "../react-ui/build", "index.html"));
+});
 
-  // Priority serve any static files.
-  app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
-
-  // Answer API requests.
-  app.get('/api', function (req, res) {
-    res.set('Content-Type', 'application/json');
-    res.send('{"message":"Hello from the custom server!"}');
-  });
-
-  // All remaining requests return the React app, so it can handle routing.
-  app.get('*', function(request, response) {
-    response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
-  });
-
-  app.listen(PORT, function () {
-    console.error(`Node cluster worker ${process.pid}: listening on port ${PORT}`);
-  });
-}
+app.listen(PORT, function() {
+  console.error(`Server listening on port ${PORT}`);
+});
